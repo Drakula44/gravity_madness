903a8a666792d14a698c9b0af1698c18a7fc752a
Drakula44 <ninadrakulic04@gmail.com>
2020-02-16 16:38:35 +0100

    dodata gravitacija

---
+++ Assets/Code/Gravity.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    private static List<Gravity> objekti = new List<Gravity>();
+    public Rigidbody2D telo;
+    public float ugao;
+    void Start()
+    {
+        objekti.Add(this);
+    }
+
+    void Update()
+    {
+
+        for(int i = 0;i < objekti.Count;i++)
+        {
+            if(objekti[i] == this)
+                continue;
+            float G = -1;
+            float force = G*telo.mass*objekti[i].telo.mass/Vector3.Distance(transform.position,objekti[i].transform.position);
+            ugao = Mathf.Atan2((transform.position.y-objekti[i].transform.position.y),(transform.position.x-objekti[i].transform.position.x));
+            telo.AddForce(new Vector2(Mathf.Cos(ugao)*force,Mathf.Sin(ugao)*force));
+        }
+
+    }
+}
---
+++ Assets/Code/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = new Vector3();
+        float dp = 0.1F;
+        if(Input.GetKey("w"))
+        {
+            direction.y = dp;
+        }
+        if(Input.GetKey("s"))
+        {
+            direction.y = -dp;
+        }
+        if(Input.GetKey("a"))
+        {
+            direction.x = -dp;
+        }
+        if(Input.GetKey("d"))
+        {
+            direction.x = dp;
+        }
+        transform.Translate(direction,Space.World);
+    }
+}
